9ecf34acb74642d210879876ae42c5f7566c3d41
Rikard Engström <rikardengstrom89@gmail.com>
2024-09-09 19:01:43 +0200

    almost done

--- button.js
+++ button.js
@@ -1,3 +1,3 @@
-function textChange() {
-    document.getElementById("heading").innerHTML = "Goodbye World"
+function textChange() { //Funktion skapad med namnet textChange.
+    document.getElementById("heading").textContent = "Goodbye World" //Hittar elementet ID "Heading" och ändrar texten inuti elementet till "Goodbye World"
 }
