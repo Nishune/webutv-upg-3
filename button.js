6a082ff748ec77f75698866ed1de5c0f96e33fc8
Rikard Engström <rikardengstrom89@gmail.com>
2024-09-12 12:32:49 +0200

    fixat kodkommentar

--- button.js
+++ button.js
@@ -1,3 +1,3 @@
 function textChange() { //Funktion skapad med namnet textChange.
-    document.querySelector("#heading").innerHTML = "Goodbye World" //Hittar elementet ID "Heading" och ändrar texten inuti elementet till "Goodbye World"
+    document.querySelector("#heading").innerHTML = "Goodbye World" //Hittar elementet med ID "Heading" och ändrar texten inuti elementet till "Goodbye World"
 }
